972582ebc64e442fba9b0ec3a92843a4f86d2040
Faridah Salami <f.salami@outlook.com>
2024-04-04 12:44:25 +0100

    initial commit but final as code was written in nology repo before copied over

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
 import "./styles/style.scss";
+import { pokemonArray } from './data/pokemon';
+import { Pokemon } from "./data/types";
+
+
+const cardContainer = document.querySelector<HTMLElement>(".card-container");
+const pokemonFilter = document.querySelector<HTMLInputElement>("#pokemon__filter");
+const filterButton = document.querySelector<HTMLButtonElement>(".pokemon__filter-button");
+
+if(!cardContainer || !pokemonFilter ||!filterButton){
+    throw new Error ("Issue with selector")
+}
+
+const handleRenderCards = (pokemonArray: Pokemon[]) => {
+    
+    const pokemons = Object.values(pokemonArray);
+    cardContainer.innerHTML ="";
+    pokemons.forEach((pokemon: Pokemon) => {
+        cardContainer.innerHTML += `
+        <div class="card">
+        <img class="card__image" src=${pokemon.sprite}>
+        <div class="card__content">
+        <h3 class="card__heading" style="text-transform: capitalize">${pokemon.name}</h3> 
+        <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} & ${pokemon.types[1]} type pokemon.</p>
+        </div>
+        </div`;
+    })
+}
+
+const handleFilter = () => {
+    const searchTerm = pokemonFilter.value.toLowerCase();
+    const filteredPokemons = pokemonArray.filter((pokemon) =>{
+        if(pokemon.name.toLowerCase().includes(searchTerm) || String(pokemon.id).includes(searchTerm)){
+            return true
+        }else {
+            return false
+        }
+    })
+    handleRenderCards(filteredPokemons);
+}
+
+cardContainer.addEventListener("load", handleRenderCards(pokemonArray));
+filterButton.addEventListener("click", handleFilter);
